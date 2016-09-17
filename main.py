dabea6666e2c64b5b824ae3db0bb4e7467381c38
Artem Kondyukov <artemkondyukov@gmail.com>
2016-09-17 17:31:09 +0300

    basic

--- main.py
+++ main.py
@@ -6,9 +6,12 @@ with open("./sensitive") as f:
 bot = telebot.TeleBot(BOT_TOKEN)
 
 
-@bot.message_handler(content_types=["text"])
+@bot.message_handler(content_types=["text", "location"])
 def repeat_all_messages(message):
-    bot.send_message(message.chat.id, message.text)
+    if message.content_type == "location":
+        bot.send_message(message.chat.id, str(message.location))
+    elif message.content_type == "text":
+        bot.send_message(message.chat.id, message.text)
 
 if __name__ == '__main__':
     bot.polling(none_stop=True)
