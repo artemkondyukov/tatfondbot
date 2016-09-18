bf4c7fdeeb17581dbb39b95d860c858fc45b128b
Artem Kondyukov <artemkondyukov@gmail.com>
2016-09-18 11:39:44 +0300

    skype returns lat and lon

--- skypemain.py
+++ skypemain.py
@@ -1,10 +1,40 @@
-import Skype4Py
+# import Skype4Py
+#
+# skype = Skype4Py.Skype()
+#
+# skype.Attach()
+#
+# print ('Your full name:', skype.CurrentUser.FullName)
+# print ('Your contacts:')
 
-skype = Skype4Py.Skype()
+from skpy import Skype
+from skpy import SkypeEventLoop, SkypeMessageEvent, SkypeNewMessageEvent, SkypeLocationMsg
 
-skype.Attach()
 
-print ('Your full name:', skype.CurrentUser.FullName)
-print ('Your contacts:')
-for user in skype.Friends:
-    print ('    ', user.FullName)
+# sk = Skype("andreevich_94", "55d52d33f3a")
+#
+# for user in sk.contacts:
+#     print('    ', user.Name)
+
+
+class SkypePing(SkypeEventLoop):
+    def __init__(self):
+        super(SkypePing, self).__init__("andreevich_94", "55d52d33f3a")
+
+    def onEvent(self, event):
+        # print(event)
+        if isinstance(event, SkypeMessageEvent):
+            if isinstance(event.msg, SkypeLocationMsg):
+                # print(dir(event.msg))
+                print(event.msg.latitude)
+                print(event.msg.longitude)
+            # elif isinstance(event, SkypeLocationMsg):
+            #         # and not event.userId == self.userId:
+            #     print(event)
+            # print(123)
+            # print(event)
+
+
+sp = SkypePing()
+print(123)
+sp.loop()
